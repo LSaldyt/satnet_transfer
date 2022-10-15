31b3974a6cbdfa5720194af238cb3db5ba54dd04
LSaldyt <lucassaldyt@gmail.com>
2022-10-15 16:35:55 -0700

    Create a run-all script in main.py

--- scripts/create_selected_datasets.py
+++ scripts/create_selected_datasets.py
@@ -12,16 +12,13 @@ selected_problems = [
     'PLANNING/BlocksWorld/blocksworld',
     'PLANNING/Logistics/logistics',
     'DIMACS/AIM/aim',
-    # 'DIMACS/PARITY/parity',
+    'DIMACS/PARITY/parity',
     'DIMACS/HANOI/hanoi',
     'DIMACS/PHOLE/pigeon-hole',
     'Bejing/Bejing',
     'GCP/gcp-large',
     'QG/QG'
         ]
-# selected_problems = ['DIMACS/PARITY/parity']
-# selected_problems = ['CBS/CBS_k3_n100_m403_b10']
-selected_problems = ['PLANNING/BlocksWorld/blocksworld']
 selected_problems = [(f'{base_url}{prob}{ext}', PurePath(f'{prob}{ext}'))
                      for prob in selected_problems]
 
@@ -36,18 +33,19 @@ def run():
     data_dir.mkdir(exist_ok=True)
     rng = np.random.default_rng(2022)
 
-    transforms = make_transforms(rng, size=1)
+    transforms = make_transforms(rng, size=10)
 
     for url, path in selected_problems:
         stem = path.with_suffix('').stem
         extract_dir = cnf_dir / Path(stem)
         extract_dir.mkdir(exist_ok=True)
         download_and_extract(url=url, target_path=path.name, extract_dir=extract_dir)
-        for filename in extract_dir.glob(f'*.cnf'):
-            print(filename)
-            for transform_name, transform in transforms:
-                dataset_path = generate_dataset(filename, data_dir, rng,
-                                                transform=transform, limit=10000,
-                                                suffix=f'_{transform_name}.npz')
-            break
-        break
+        try:
+            for filename in extract_dir.glob(f'*.cnf'):
+                print(filename)
+                for transform_name, transform in transforms:
+                    dataset_path = generate_dataset(filename, data_dir, rng,
+                                                    transform=transform, limit=10000,
+                                                    suffix=f'_{transform_name}.npz')
+        except SolverFailedError:
+            print(f'Solving failed for {stem}, skipping.')
--- scripts/main.py
+++ scripts/main.py
@@ -7,20 +7,24 @@ from satnet_transfer.settings import Settings
 from pathlib import Path
 
 def run():
-    s = Settings(batch_size=32, lr=2e-3, epochs=10, split=0.8, metrics_file='metrics/basic.csv')
     rng = np.random.default_rng(2022)
+    s   = Settings(batch_size=32, lr=2e-3, epochs=10, split=0.8)
 
     metrics_dir = Path('metrics')
     metrics_dir.mkdir(exist_ok=True)
 
-    dataset_path = Path('data/CBS_k3_n100_m403_b10_999_masked.npz')
-    dataset = SATDataset(dataset_path)
+    data_dir = Path('data/')
+    for problem_file in data_dir.glob('*.npz'):
+        stem = problem_file.stem
+        s.update(metrics_file=f'metrics/{stem}.csv')
 
-    inp, inp_mask, lbl = dataset[0]
-    n   = inp.shape[0]
-    m   = 100
-    aux = 10
-    sat = satnet.SATNet(n, m, aux)
+        dataset = SATDataset(problem_file)
 
-    optimizer = torch.optim.AdamW(sat.parameters(), lr=s.lr)
-    loop(sat, dataset, optimizer, s)
+        inp, inp_mask, lbl = dataset[0]
+        n   = inp.shape[0]
+        m   = 1000
+        aux = 1000
+        sat = satnet.SATNet(n, m, aux)
+
+        optimizer = torch.optim.AdamW(sat.parameters(), lr=s.lr)
+        loop(sat, dataset, optimizer, s)
